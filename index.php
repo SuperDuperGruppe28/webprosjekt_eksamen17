9ee874932b4ad6a10fbd7088ec3a15345b071879
Seebzei <seebzei@gmail.com>
2017-03-23 13:36:00 +0100

    La til cssmappe

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        
+        <link rel="stylesheet" type="text/css" href="css/main.css">
 
-echo "Hei";
+        <title>Eksamen</title>
+
+    </head>
+    <body>
+        <?= "HALLOO";?>
+    </body>
+</html>
