0c6cd11a51f70f57a09c9b16ea26b61aa42ab15b
Seb <seebzei@gmail.com>
2017-03-29 13:19:56 +0200

    Satte opp composer med laravel

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        
-        <link rel="stylesheet" type="text/css" href="css/main.css">
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
 
-        <title>Eksamen2017</title>
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
-    </head>
-    <body>
-        <?= "HALLLOOOOOOOOOOOOOOOOOOOO";?>
-    </body>
-</html>
+// create a log channel
+$log = new Logger('G28');
+$log->pushHandler(new StreamHandler('logs/test.banan', Logger::WARNING));
+// add records to the log
+$log->warning('SuperTest');
+$log->error('Veldig farlig');
+
+echo 'Skapte log for å teste Composer med laravel';
