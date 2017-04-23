e65e8f3f6ea49660d55f76ccea5533453349c209
Seb <seebzei@gmail.com>
2017-04-23 11:46:54 +0200

    La til flere funksjoner i Bruker.php

--- database/tools/bruker.php
+++ database/tools/bruker.php
@@ -26,4 +26,63 @@ function eksistererBruker($bruker)
     return (Bruker::find($bruker)) !== null ? true : false;
 }
 
-registrerBruker("Mamammllama", "ss", "saa", 1);
+// Returnerer emailen til gitt bruker
+function hentEmail($bruker)
+{
+    if(eksistererBruker($bruker))
+       return Bruker::find($bruker)->Email;
+    return null;
+}
+
+// Returnerer om gitt bruker er administrator eller ikke
+function erAdmin($bruker)
+{
+    if(eksistererBruker($bruker))
+    {
+        return Bruker::find($bruker)->Admin > 0 ? true : false;
+    }
+    return false;
+}
+
+// Setter gitte bruker til admin, 0 = ikke admin > 1 = admin
+function settAdmin($bruker, $admin)
+{
+    if(eksistererBruker($bruker))
+    {
+        $bru = Bruker::find($bruker);
+        $bru->Admin = $admin;
+        $bru->save();
+        
+        return true;
+    }
+    return false;
+}
+
+// Returnerer om gitt bruker er verifisert eller ikke
+function erVerifisert($bruker)
+{
+    if(eksistererBruker($bruker))
+    {
+        return Bruker::find($bruker)->Verifisert > 0 ? true : false;
+    }
+    return false;
+}
+
+// Setter gitte bruker verifisert 0 = ikke verifisert > 1 = verifisert
+function settVerifisert($bruker, $veri)
+{
+    if(eksistererBruker($bruker))
+    {
+        $bru = Bruker::find($bruker);
+        $bru->Verifisert = $veri;
+        $bru->save();
+        
+        return true;
+    }
+    return false;
+}
+
+// Genere mailverificationhash
+
+// Sette mail verified
+
--- index.php
+++ index.php
@@ -1,60 +1,15 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/database/models.php';
+require_once __DIR__ . '/database/tools/bruker.php';
 
 
-echo '<pre>';
+if(eksistererBruker("seb"))
+    echo "Brukeren seb eksisterer yo";
 
-$a = Deltagelse::find(1);
-$a->Deltagelse = 12;
-$a->save();
-// Henter ut brukeren som opprette aktivitet nummer 1
-foreach (Stemmer::all() as $ak)
-{
-    print_r($ak->brukere);
-    //echo ($ak->brukere->Brukernavn);
-}
+echo hentEmail("seab");
 
+settAdmin("seb", 1);
 
-
-/*
-
-// SUPERTESTER
-
-echo '<pre>';
-foreach (Bruker::all() as $brukere)
-{
-    //print_r($brukere->tags);
-    echo ($brukere->tags->Tag);
-}
-
-echo '<pre>';
-
-print_r(Bruker::where("Brukernavn", "LIKE", "%a%")->get());
-
-echo '</pre>';
-
-$bru = Bruker::find("seb")->Brukernavn;
-
-$TagsbrukerForste = TagsBruker::all()->where("Bruker", "LIKE", $bru);
-//echo dd($TagsbrukerForste);
-foreach ($TagsbrukerForste as $tags)
-{
-   echo $tags->Bruker . " " . $tags->Tag . "<br>";
-}
-
-$TagsbrukerAlle = TagsBruker::all();
-//echo dd($TagsbrukerForste);
-foreach ($TagsbrukerAlle as $tags)
-{
-   echo $tags->Bruker . " " . $tags->Tag . "<br>";
-}
-
-
-echo '<pre>';
-foreach (TagsBruker::all() as $tags)
-{
-    print_r($tags->brukere);
-    print_r($tags->tags);
-}
-*/
+if(erAdmin("seb"))
+    echo "admin";
+else 
+    echo "ikke admin";
