8f7daf30683eb8284236ca8aef00e6b54ad8c39c
Unknown <poljac@mail.com>
2017-04-28 11:55:38 +0200

    fix head

--- pages/main.php
+++ pages/main.php
@@ -7,7 +7,6 @@
         <meta name="description" content="Veldig bra side">
         <meta name="author" content="Gruppe28">
         <title><?php echo pathinfo(__FILE__, PATHINFO_FILENAME);?></title>
-        <meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="../assets/css/main.css" />
