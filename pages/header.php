cc582410553c85e0355a29ee2d30b9c190f0b5cf
Unknown <poljac@mail.com>
2017-05-04 13:28:17 +0200

    meny

--- pages/header.php
+++ pages/header.php
@@ -1,3 +1,32 @@
 <div id="headerContainer">
-    <center><h1>Hello</h1></center>
+    <center>
+      
+        <nav>
+    <ul>
+        <li><a href="?side=Home">Hjem</a></li>
+        <li>
+            <a href="?side=Activities">Aktiviteter <span class="caret"></span></a>
+            <div>
+                <ul>
+                    <li>
+                        <a href="?side=Activities">Ting <span class="caret"></span></a>
+                        <div>
+                            <ul>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                <li><a href="#">5</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="?side=test">Tang</a></li>
+                </ul>
+            </div>
+        </li>
+        <li><a href="?side=test">Om Oss</a></li>
+        <li><a href="?side=test">Test</a></li>
+    </ul>
+</nav>
+    </center>
 </div>
