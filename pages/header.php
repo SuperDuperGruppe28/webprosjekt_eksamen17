31e2bc819213c35b0ab2f901cbcc6d081db6d0f1
Unknown <poljac@mail.com>
2017-05-04 13:50:55 +0200

    aktiviteter

--- pages/header.php
+++ pages/header.php
@@ -1,7 +1,12 @@
+<?php
+require_once __DIR__ . '/../database/tools/bruker.php';
+require_once __DIR__ . '/../database/tools/kommentar.php';
+?>
+
 <div id="headerContainer">
     <center>
       
-        <nav>
+    <nav>
     <ul>
         <li><a href="?side=Home">Hjem</a></li>
         <li>
@@ -12,6 +17,7 @@
                         <a href="?side=Activities">Ting <span class="caret"></span></a>
                         <div>
                             <ul>
+                                
                                 <li><a href="#">1</a></li>
                                 <li><a href="#">2</a></li>
                                 <li><a href="#">3</a></li>
@@ -21,6 +27,12 @@
                         </div>
                     </li>
                     <li><a href="?side=test">Tang</a></li>
+                    <?php 
+                                
+foreach(hentKommentarer(3) as $kom) 
+{
+    echo '<li><a href="#">' . $kom->Bruker . ": " . $kom->Tekst . '</a></li>';
+}                               ?>
                 </ul>
             </div>
         </li>
