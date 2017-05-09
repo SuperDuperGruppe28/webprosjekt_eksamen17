24501d13cb41c55d9dbf626bba2c53b163bb1de5
Seebzei <seebzei@gmail.com>
2017-05-09 12:47:40 +0200

    La til registreringsside for aktivitet

--- pages/aktivitet.php
+++ pages/aktivitet.php
@@ -21,7 +21,31 @@
             }
         }else
         {
+            $brukernavn = loggetInnBruker();
+            if($brukernavn)
+            {?>
+             <h1>Lag ny aktivitet</h1>
+        <form action="php/activity.php?action=reg" method="post">
+            <label for="tittel">Tittel</label> <input type="text" id="tittel" name="tittel"><br/><br/>
+            <label for="beskrivelse">Beskrivelse</label> <textarea id="beskrivelse" name="beskrivelse" rows="10" cols="10"></textarea><br/><br/>
+            <label for="apning">Åpning</label> <input type="text" id="apning" name="apning"><br/><br/>
+            <label for="dato">Dato</label> <input type="datetime-local" name="dato" id="dato"><br><br>
+            <label for="pris">Pris</label> <input type="number" id="pris" name="pris"><br/><br/>
+            <label for="bilde">Bilde</label> <input type="text" id="bilde" name="bilde"><br/><br/>
+            <input type = "hidden" id = "lengdegrad" name="lengdegrad" value = "0" />
+            <input type = "hidden" id = "breddegrad" name="breddegrad" value = "0" />
             
+            <?php
+            if(erAdmin($brukernavn))
+                echo '<label for="statisk">Statisk</label> <input type="number" id="statisk" name="statisk"><br/><br/>';    
+            ?>
+            <input class="button" type="submit" value="Registrer aktivitet"/>
+        </form>
+    <?php
+            }else
+            {
+                echo "<h1>Logg inn for å opprette en ny aktivitet!";
+            }
         }
     ?>
 
