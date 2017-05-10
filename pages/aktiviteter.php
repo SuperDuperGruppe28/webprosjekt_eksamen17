64ce1400e412d47afe322ca566a50c552258bb0a
Espen Sotnakk <espen.sotnakk@gmail.com>
2017-05-10 15:07:01 +0200

    dette blir konge gutta
    aiai

--- pages/aktiviteter.php
+++ pages/aktiviteter.php
@@ -4,3 +4,76 @@
             $id = $_GET['tag'];
         }
 ?>
+
+<h3>Aktiviteter</h3>
+
+<div id="katogorier">
+    <p>Resturanter</p>
+    <p>Café</p>
+    <p>Pub</p>
+    <p>Natur</p>
+    <p>Sport</p>
+</div>
+
+<div id="topleft">
+    <img id="vannbilde" src="/img/COLOURBOX2340325.jpg" alt="Vann"> 
+    <div id="textbox">
+           <div>
+               <p>POKALEN PUB</p>
+                <span >Kun det beste innen sport på fjernsyn, musikk og godt drikke. Pokalen viser det aller meste av kamper fra Premier League, Champions league, utvalgte godbiter NFL, hockey, skiskytning, hopprenn, Sundby vs Northug og annen idrett. 10 skjermer som hengyer høyt nok til at alle ser, tre soner, bra lyd, digge sofaer og et herlig utvalg av godt drikke. Både flaske og tapp.</span ></br>
+           </div>  
+    </div>
+</div>
+
+<div id="topcenter">
+    <img id="vannbilde" src="/img/COLOURBOX2340325.jpg" alt="Vann"> 
+
+        <div id="textbox">
+           <div>
+               <p>POKALEN PUB</p>
+             <span >Kun det beste innen sport på fjernsyn, musikk og godt drikke. Pokalen viser det aller meste av kamper fra Premier League, Champions league, utvalgte godbiter NFL, hockey, skiskytning, hopprenn, Sundby vs Northug og annen idrett. 10 skjermer som hengyer høyt nok til at alle ser, tre soner, bra lyd, digge sofaer og et herlig utvalg av godt drikke. Både flaske og tapp.</span ></br>
+           </div>  
+    </div>
+</div>
+
+<div id="topright">
+    <img id="vannbilde" src="/img/COLOURBOX2340325.jpg" alt="Vann"> 
+
+        <div id="textbox">
+           <div>
+               <p>POKALEN PUB</p>
+             <span >Kun det beste innen sport på fjernsyn, musikk og godt drikke. Pokalen viser det aller meste av kamper fra Premier League, Champions league, utvalgte godbiter NFL, hockey, skiskytning, hopprenn, Sundby vs Northug og annen idrett. 10 skjermer som hengyer høyt nok til at alle ser, tre soner, bra lyd, digge sofaer og et herlig utvalg av godt drikke. Både flaske og tapp.</span ></br>
+           </div>  
+    </div>
+</div>
+
+<div id="bottomleft">
+    <img id="vannbilde" src="/img/COLOURBOX2340325.jpg" alt="Vann"> 
+
+        <div id="textbox">
+           <div>
+               <p>POKALEN PUB</p>
+             <span >Kun det beste innen sport på fjernsyn, musikk og godt drikke. Pokalen viser det aller meste av kamper fra Premier League, Champions league, utvalgte godbiter NFL, hockey, skiskytning, hopprenn, Sundby vs Northug og annen idrett. 10 skjermer som hengyer høyt nok til at alle ser, tre soner, bra lyd, digge sofaer og et herlig utvalg av godt drikke. Både flaske og tapp.</span ></br>
+           </div>  
+    </div>
+</div>
+
+<div id="bottomcenter">
+    <img id="vannbilde" src="/img/COLOURBOX2340325.jpg" alt="Vann"> 
+        <div id="textbox">
+           <div>
+               <p>POKALEN PUB</p>
+             <span >Kun det beste innen sport på fjernsyn, musikk og godt drikke. Pokalen viser det aller meste av kamper fra Premier League, Champions league, utvalgte godbiter NFL, hockey, skiskytning, hopprenn, Sundby vs Northug og annen idrett. 10 skjermer som hengyer høyt nok til at alle ser, tre soner, bra lyd, digge sofaer og et herlig utvalg av godt drikke. Både flaske og tapp.</span ></br>
+           </div>  
+    </div>
+</div>
+
+<div id="bottomright">
+    <img id="vannbilde" src="/img/COLOURBOX2340325.jpg" alt="Vann"> 
+        <div id="textbox">
+           <div>
+               <p>POKALEN PUB</p>
+             <span >Kun det beste innen sport på fjernsyn, musikk og godt drikke. Pokalen viser det aller meste av kamper fra Premier League, Champions league, utvalgte godbiter NFL, hockey, skiskytning, hopprenn, Sundby vs Northug og annen idrett. 10 skjermer som hengyer høyt nok til at alle ser, tre soner, bra lyd, digge sofaer og et herlig utvalg av godt drikke. Både flaske og tapp.</span ></br>
+           </div>  
+    </div>
+</div>
