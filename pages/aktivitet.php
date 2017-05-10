97ba43e7978d2c833f3e1d5b0a7a777ee095883e
Seebzei <seebzei@gmail.com>
2017-05-10 14:14:08 +0200

    La til en 'Stemmeknapp på aktivitetsside'

--- pages/aktivitet.php
+++ pages/aktivitet.php
@@ -60,9 +60,10 @@
                             echo "<b>" . $tag->Tag . " = " . $tag->Vekt . "%</b>, ";
                         }
                 echo "Antall stemmer: " . antallStemmer($id);
-                echo '<form action="/php/activity.php?action=stem&akti='. $id .'">
+                ?><form action="php/activity.php?action=stem&akti=<?=$id?>" method="post">
                         <input type="submit" value="Stem!" />
-                </form>';
+                </form>
+                <?php
                 echo "<div id='map'></div>";
                 
                 //<!--Laste google maps-->
