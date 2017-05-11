9d37135163953d45f7c69945eae86048aee140ab
Koppang <knut.koppang@gmail.com>
2017-05-11 12:44:50 +0200

    k

--- pages/header.php
+++ pages/header.php
@@ -53,10 +53,11 @@
                 </li>
                 <li><a href="?side=aktivitet">Lag aktivitet</a></li>
                 <li>
-                    <form id="searchbox" action="">
+                    <form id="searchbox" style='vertical-align: middle'>
                         <input id="search" type="text" placeholder="Søk her . . .">
                         <input id="submit" type="submit" value="Søk">
                     </form>
+
                 </li>
                 <?php
             if(erBrukerLoggetInn())
