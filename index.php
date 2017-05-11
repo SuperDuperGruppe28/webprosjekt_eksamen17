61b0eb59cb22d78d5baefb87821adc3f63e5ca55
Espen Sotnakk <espen.sotnakk@gmail.com>
2017-05-11 13:29:56 +0200

    Social media links
    facebook, twitter og instagram linker til westerdals sider og åpner
    linken i ny tab

--- index.php
+++ index.php
@@ -67,9 +67,9 @@ if(isset($_GET[$GSide]))
             
         </div>
     
-            <a href="#" class="fa fa-facebook"></a>
-            <a href="#" class="fa fa-twitter"></a>
-            <a href="#" class="fa fa-instagram"></a>
+            <a href="https://www.facebook.com/westerdalsosloact/" target="_blank" class="fa fa-facebook"></a>
+            <a href="https://twitter.com/westerdals?lang=no" target="_blank" class="fa fa-twitter"></a>
+            <a href="https://www.instagram.com/westerdalsact/" target="_blank" class="fa fa-instagram"></a>
             <!-- FOOTER -->
         <footer><?php require 'pages/footer.php'?></footer>
     </body>
