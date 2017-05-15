4e2505a2f1378afed771f317d32d0c9e34ab5454
Espen Sotnakk <espen.sotnakk@gmail.com>
2017-05-15 13:40:34 +0200

    Footer /m link
    osv

--- pages/footer.php
+++ pages/footer.php
@@ -1,8 +1,5 @@
 <div id="footerContainer">    
         <div id="sosialemedier">
-            <a href="https://www.facebook.com/westerdalsosloact/" target="_blank" class="fa fa-facebook"></a>
-            <a href="https://twitter.com/westerdals?lang=no" target="_blank" class="fa fa-twitter"></a>
-            <a href="https://www.instagram.com/westerdalsact/" target="_blank"></a> <img src="/img/instagram.png" width="30" height="30"></img>
             <center>SuperDuperGruppe28(c) 2017</center>
         </div>
 </div>
