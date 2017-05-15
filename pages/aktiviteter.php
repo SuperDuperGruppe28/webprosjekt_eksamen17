97351e0acc256d1b8d150f27a7d42cc280a7b7d2
Unknown <poljac@mail.com>
2017-05-15 14:42:08 +0200

    center aktiviteter

--- pages/aktiviteter.php
+++ pages/aktiviteter.php
@@ -2,20 +2,24 @@
         if(isset($_GET['tag']))
         {
             $tag = $_GET['tag'];
-            
+            echo "<center>";
             echo "<h3>Aktiviteter fra ".$tag."</h3>";
+            
             
             foreach(hentAktiviteterFraTag($tag) as $akt)
             {
                 printAktivitetBoks($akt->id);
             }
+            echo "</center>";
         }else
         {
             // printer alle aktivitetene
+            echo "<center>";
             echo "<h3>Aktiviteter</h3>";
             foreach(hentAlleAktiviteter() as $akt)
             {
                 printAktivitetBoks($akt->id);
             }
+            echo "</center>";
         }
 ?>
