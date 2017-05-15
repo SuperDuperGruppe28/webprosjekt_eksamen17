5b31a1d6dda5644c0f84d1101f083c283e2c1ec2
Unknown <poljac@mail.com>
2017-05-16 00:08:51 +0200

    max aktiviteter

--- pages/header.php
+++ pages/header.php
@@ -13,42 +13,56 @@
                     <a href="?side=aktiviteter">Aktiviteter<span class="caret"></span></a>
                     <div>
                         <ul>
-                            <?php                                 
-                        foreach(Tags::All() as $tag) 
-                        {
-                            echo '<li><a href="?side=aktiviteter&tag='.$tag->Tag.'">' . $tag->Tag . '<span class="caret"></span></a>';
-                            echo '<div><ul>';
-                            
-                            $aktiviteter = TagsAktivitet::where("Tag", "=", $tag->Tag)->get();
-                
-                            foreach($aktiviteter as $aktivitet) {
-                                $href = "?side=aktivitet&id=" . $aktivitet->Aktivitet;
-                                $aktivitet = hentAktivitet($aktivitet->Aktivitet);
-                                
-                                $deltagere = $aktivitet->deltagere()->get();
-                                $deltar = false;
-                                foreach($deltagere as $deltager)
-                                {
-                                    if ($deltager->Bruker == loggetInnBruker()) {
-                                        $deltar = true;
-                                        break;
+                            <?php
+
+                            $tags = Tags::all();
+                            $tags_count = $tags->count();
+                            $limit = 5;
+
+                            $tags_limit = min($tags_count, $limit);
+
+                            for ($i=0; $i<$tags_limit; $i++)
+                                //foreach(Tags::All() as $tag)
+                            {
+                                $tag = $tags[$i];
+                                echo '<li><a href="?side=aktiviteter&tag='.$tag->Tag.'">' . $tag->Tag . '<span class="caret"></span></a>';
+                                echo '<div><ul>';
+
+                                $aktiviteter = TagsAktivitet::where("Tag", "=", $tag->Tag)->get();
+
+                                foreach($aktiviteter as $aktivitet) {
+                                    $href = "?side=aktivitet&id=" . $aktivitet->Aktivitet;
+                                    $aktivitet = hentAktivitet($aktivitet->Aktivitet);
+
+                                    $deltagere = $aktivitet->deltagere()->get();
+                                    $deltar = false;
+                                    foreach($deltagere as $deltager)
+                                    {
+                                        if ($deltager->Bruker == loggetInnBruker()) {
+                                            $deltar = true;
+                                            break;
+                                        }
+                                    }
+
+                                    //echo '<li><a href="'.$href.'">' . $aktivitet->Tittel . '</li>';
+                                    $href = "?side=aktivitet&id=" . $aktivitet->id;
+                                    if($deltar)
+                                    {
+                                        echo '<li><a id="delta" href="'. $href .'">' . $aktivitet->Tittel . '</a></li>';
+                                    } else
+                                    {
+                                        echo '<li><a href="'. $href .'">' . $aktivitet->Tittel . '</a></li>';
                                     }
                                 }
-                                
-                                //echo '<li><a href="'.$href.'">' . $aktivitet->Tittel . '</li>';
-                                $href = "?side=aktivitet&id=" . $aktivitet->id;
-                                if($deltar)
-                                {
-                                    echo '<li><a id="delta" href="'. $href .'">' . $aktivitet->Tittel . '</a></li>';
-                                } else
-                                {
-                                    echo '<li><a href="'. $href .'">' . $aktivitet->Tittel . '</a></li>';
-                                }
+
+                                echo '</ul></div></li>';
+
                             }
-                            
-                            echo '</ul></div></li>';
-                            
-                        }?>
+                            // vis alle aktiviteter knapp hvis det er flere enn $limit
+                            if ($tags_count > $limit) {
+                                echo '<li><a href="?side=aktiviteter" class="cursive">vis alle</a></li>';
+                            }
+                            ?>
                         </ul>
                     </div>
                 </li>
