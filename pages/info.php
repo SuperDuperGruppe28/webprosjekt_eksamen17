b19f6d0e9b8756cbf5663e0799a0c9096e76b0b7
Seebzei <seebzei@gmail.com>
2017-05-24 14:23:00 +0200

    La til mer info infoside

--- pages/info.php
+++ pages/info.php
@@ -1,7 +1,16 @@
 <div id="mainBoks">
     <h1>Vulkanelva</h1>
     <p>Hallo og velkommen til Vulkanelva, her vil du finne alt av aktiviteter og ting en kan bedrive tiden med i næromrodet til Campus Vulkan!</p>
-
+    
+    
+    <h3>Utviklere</h3>
+    <p>
+        Christian<br>
+        Espen<br>
+        Knut<br>
+        Tobias<br>
+        Sebastian<br>
+    </p>
     <h3>Her finner du oss!</h3>
     <div id="map"></div>
 </div>
