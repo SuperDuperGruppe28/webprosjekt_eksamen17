a03bd02b2e3123824d48b818525f2a2fea80dd58
Espen Sotnakk <espen.sotnakk@gmail.com>
2017-05-24 14:04:29 +0200

    fix på footer

--- pages/footer.php
+++ pages/footer.php
@@ -3,7 +3,11 @@
         <a href="https://www.facebook.com/westerdalsosloact/" target="_blank" class="so"><img src="img/facebook.png" width="25" height="25" style="border: 1px solid black"></a>
         <a href="https://twitter.com/westerdals?lang=no" target="_blank" class="so"><img src="img/twitter.png" width="25" height="25" style="border: 1px solid black"></a>
         <a href="https://www.instagram.com/westerdalsact/" target="_blank" class="so"><img src="img/instagram.png" width="25" height="25" style="border: 1px solid black"></a>
-        <center>SuperDuperGruppe28(c) 2017</center>
+    </div>
+        <div id="copyright">
+            SuperDuperGruppe28©2017
+        </div>
+    <div id="kontakt">
         <a href="mailto:kopknu16@student.westerdals.no?subject=Tilbakemelding om Vulkanelva">Kontakt Oss</a>
     </div>
 </div>
