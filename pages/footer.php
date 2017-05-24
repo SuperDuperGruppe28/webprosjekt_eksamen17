15d3fd9b7b5dda8d03e9e83449ec876227554cfa
Espen Sotnakk <espen.sotnakk@gmail.com>
2017-05-24 14:33:26 +0200

    fix sosiale medier
    border-radius

--- pages/footer.php
+++ pages/footer.php
@@ -1,8 +1,8 @@
 <div id="footerContainer">
     <div id="sosialemedier">
-        <a href="https://www.facebook.com/westerdalsosloact/" target="_blank" class="so"><img src="img/facebook.png" width="25" height="25" style="border: 1px solid black"></a>
-        <a href="https://twitter.com/westerdals?lang=no" target="_blank" class="so"><img src="img/twitter.png" width="25" height="25" style="border: 1px solid black"></a>
-        <a href="https://www.instagram.com/westerdalsact/" target="_blank" class="so"><img src="img/instagram.png" width="25" height="25" style="border: 1px solid black"></a>
+        <a href="https://www.facebook.com/westerdalsosloact/" target="_blank" class="so"><img src="img/facebook.png" width="25" height="25" style="border-radius: 3px"></a>
+        <a href="https://twitter.com/westerdals?lang=no" target="_blank" class="so"><img src="img/twitter.png" width="25" height="25" style="border-radius: 3px"></a>
+        <a href="https://www.instagram.com/westerdalsact/" target="_blank" class="so"><img src="img/instagram.png" width="25" height="25" style="border-radius: 3px"></a>
     </div>
         <div id="copyright">
             SuperDuperGruppe28©2017
