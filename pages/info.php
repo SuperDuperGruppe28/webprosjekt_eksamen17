2eb707193ddde2b02893281f0c74cbf7a734629b
Koppang <knut.koppang@gmail.com>
2017-05-25 14:10:00 +0200

    twitter test

--- pages/info.php
+++ pages/info.php
@@ -1,17 +1,40 @@
 <div id="mainBoks">
     <h1>Vulkanelva</h1>
     <p>Velkommen til Vulkanelva. Her kan du registrere deg og ta del i alle aktivitetene rundt Vulkanområdet. Om du registrerer deg vil du også få oppslag av relevante aktiviteter som vi har skreddersydd for akkurat deg! Lag deg en bruker nå, eller surf nettsiden og les om alle de spennende aktivitetene du kan ta del i!</p>
-    
+
     <h2>Utviklere</h2>
     <p>
-        Christian<br>
-        <a class="link" href="?side=bruker&id=espen">Espen</a><br>
-        <a class="link" href="?side=bruker&id=kanuto">Knut</a><br>
-        Tobias<br>
-        <a class="link" href="?side=bruker&id=sebto">Sebastian</a><br>
+        Christian
+        <br>
+        <a class="link" href="?side=bruker&id=espen">Espen</a>
+        <br>
+        <a class="link" href="?side=bruker&id=kanuto">Knut</a>
+        <br> Tobias
+        <br>
+        <a class="link" href="?side=bruker&id=sebto">Sebastian</a>
+        <br>
     </p>
     <h2>Github Repository</h2>
-    <a class="link" href="https://github.com/SuperDuperGruppe28/webprosjekt_eksamen17">Github</a><br>
+    <a class="link" href="https://github.com/SuperDuperGruppe28/webprosjekt_eksamen17">Github</a>
+    <br>
+
+    <a class="twitter-timeline" href="https://twitter.com/hashtag/babes" data-widget-id="867712070208430081">#babes Tweets</a>
+    <script>
+        ! function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                p = /^http:/.test(d.location) ? 'http' : 'https';
+            if (!d.getElementById(id)) {
+                js = d.createElement(s);
+                js.id = id;
+                js.src = p + "://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+        }(document, "script", "twitter-wjs");
+    </script>
+
+
+
+
 
     <h3>Her finner du oss!</h3>
     <div id="map"></div>
