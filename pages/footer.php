0d255f063013ff921bb5b248c9250abbc2bfe73e
Koppang <knut.koppang@gmail.com>
2017-05-25 18:13:09 +0200

    endret twitter

--- pages/footer.php
+++ pages/footer.php
@@ -1,12 +1,12 @@
 <div id="footerContainer">
     <div id="sosialemedier">
         <a href="https://www.facebook.com/westerdalsosloact/" target="_blank" class="so"><img src="img/facebook.png" width="25" height="25" style="border-radius: 3px"></a>
-        <a href="https://twitter.com/westerdals?lang=no" target="_blank" class="so"><img src="img/twitter.png" width="25" height="25" style="border-radius: 3px"></a>
+        <a href="https://twitter.com/vulkanelva?lang=no" target="_blank" class="so"><img src="img/twitter.png" width="25" height="25" style="border-radius: 3px"></a>
         <a href="https://www.instagram.com/westerdalsact/" target="_blank" class="so"><img src="img/instagram.png" width="25" height="25" style="border-radius: 3px"></a>
     </div>
-        <div id="copyright">
-            SuperDuperGruppe28©2017
-        </div>
+    <div id="copyright">
+        SuperDuperGruppe28©2017
+    </div>
     <div id="kontakt">
         <a class="link" href="mailto:kopknu16@student.westerdals.no?subject=Tilbakemelding om Vulkanelva">Kontakt oss</a>
     </div>
