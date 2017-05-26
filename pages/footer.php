1a672fc5a33b4ec4426b9822ab4284af5122ebc7
Espen Sotnakk <espen.sotnakk@gmail.com>
2017-05-26 13:58:46 +0200

    footer fix

--- pages/footer.php
+++ pages/footer.php
@@ -5,7 +5,9 @@
         <a href="https://www.instagram.com/westerdalsact/" alt="Instagram" target="_blank" class="so"><img src="img/instagram.png" width="25" height="25" style="border-radius: 3px"></a>
     </div>
     <div id="copyright">
-        SuperDuperGruppe28©2017
+        <b>"Du vil trenge mer fritid!" </b>
+        <br>
+        Gruppe 28 © 2017
     </div>
     <div id="kontakt">
         <a class="link" href="mailto:kopknu16@student.westerdals.no?subject=Tilbakemelding om Vulkanelva">Kontakt oss</a>
