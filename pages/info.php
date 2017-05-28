55a4e58a3901e4b7c64f97daf1eba6485712b0fa
Your Name <you@example.com>
2017-05-28 12:55:16 +0200

    rettet tekst på infosiden

--- pages/info.php
+++ pages/info.php
@@ -1,9 +1,9 @@
 <div class="center">
     <div id="mainBoks">
         <h1>Vulkanelva</h1>
-        <p>Velkommen til Vulkanelva. Registrer du deg her kan du opprette og ta del i aktiviteter i området rundt Vulkan.</p>
+        <p>Velkommen til Vulkanelva. Registrerer du deg her kan du opprette og ta del i aktiviteter i området rundt Vulkan.</p>
         <p>Du vil også få oppslag med relevante aktiviteter som vi har skreddersydd for akkurat deg!</p> 
-        <p> Lag deg en bruker nå, eller surf nettsiden og les om alle de spennende aktivitetene du kan ta del i!</p>
+        <p> Opprett en bruker nå eller surf nettsiden og les om alle de spennende aktivitetene du kan ta del i!</p>
         <br>
         <h2> Slik kommer du deg til Vulkanelva </h2>
         <p>Trikk 11, 12 og 13 stopper på Schous plass. Her tar man seg enkelt ned til Nordre gate og over broen til Vulkan.
@@ -12,7 +12,7 @@
                 <p> Buss 54 og 34 stopper i Møllerveien. Gå ned Møllerveien og sving inn ved Dansens Hus.</p>
 
 
-                <p> T-bane/tog: Ønsker du å ta t-bane eller tog, er det kun en 15 minutters rusletur fra Jernbanetorget </p>
+                <p> Ønsker du å ta t-bane eller tog, er det kun en 15 minutters rusletur fra Jernbanetorget </p>
 
      <div id="map"></div>
         <h3>Her finner du oss!</h3>
