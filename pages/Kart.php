04829aa7ce5fe21634e4e1627bc7f27dc6ffe4b2
Unknown <poljac@mail.com>
2017-05-28 15:09:03 +0200

    brenneriveien png->jpg

--- pages/Kart.php
+++ pages/Kart.php
@@ -18,7 +18,7 @@
         
             echo "['Fjerdingen', 59.916207, 10.759697, 0, 'img/skole_pointer.png', 'img/fjerdingen.png', 0],";
             echo "['Vulkan', 59.923393, 10.752508, 0, 'img/skole_pointer.png', 'img/vulkan.png', 0],";
-            echo "['Campus Brenneriveien', 59.920460, 10.752508, 0, 'img/skole_pointer.png', 'img/brenneriveien.png', 0],";
+            echo "['Campus Brenneriveien', 59.920460, 10.752508, 0, 'img/skole_pointer.png', 'img/brenneriveien.jpg', 0],";
             for($i = 0; $i < count($aktiviteter); $i++) {
                     $aktivitet = $aktiviteter[$i];
                     if(strtotime($aktivitet->Dato) <= time() && $aktivitet->Statisk != 1) continue; 
