edbd92058f0c30fcb7746fb48dd77c1a00466895
Unknown <poljac@mail.com>
2017-05-29 12:23:00 +0200

    feilmeldinger av

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // vis feilmeildinger
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_NONE);
 
 // PHP_verktoy
 require_once __DIR__ . '/database/tools/bruker.php';
